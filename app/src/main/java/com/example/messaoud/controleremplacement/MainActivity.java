e60b56a9cf396d61f2b71b75b0d42adb645c39ba
mmhassni <mhassni.mohammed@gmail.com>
2018-12-05 15:12:01 +0100

    third commit

--- app/src/main/java/com/example/messaoud/controleremplacement/MainActivity.java
+++ app/src/main/java/com/example/messaoud/controleremplacement/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.messaoud.controleremplacement;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<User> contacts ;
     ListView list;
     CustomAdapter adapter;
+
+    static String input = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +41,54 @@ public class MainActivity extends AppCompatActivity {
 
 
         //il sagit de la liste qu'on veut remplir (c'est une vue)
-        list = (ListView) findViewById(R.id.articleCat);
+        list = (ListView) findViewById(R.id.listViewP);
 
 
         list.setAdapter(adapter);
 
 
+        Button ajouterBoutton = (Button) this.findViewById(R.id.buttonAjouter);
 
+        TextView entree = (TextView) this.findViewById(R.id.textView);
+
+        MainActivity.input = entree.getText().toString();
+
+        ajouterBoutton.setOnClickListener(new View.OnClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+
+
+
+                contacts.add(new User(MainActivity.input,"", R.drawable.accessoire ));
+
+
+
+            }
+        });
+
+        Button supprimerBoutton = (Button) this.findViewById(R.id.buttonSupprimer);
+
+
+        supprimerBoutton.setOnClickListener(new View.OnClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+
+
+
+                contacts.add(new User(MainActivity.input,"", R.drawable.accessoire ));
+
+
+
+            }
+        });
 
 
 
     }
+
+
 }
